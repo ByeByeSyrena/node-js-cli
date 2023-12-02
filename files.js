867d927be468744abe534166458f419f4b84b3ff
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2023-12-02 19:20:30 +0200

    add getFiles and getInfo file

--- files.js
+++ files.js
@@ -1,7 +1,10 @@
 const fs = require("fs").promises;
 const path = require("path");
+const chalk = require("chalk");
 const dataValidate = require("./helpers/dataValidator");
 const checkExtension = require("./helpers/checkExtension");
+
+const folderPath = path.join(__dirname, "files");
 
 async function createFile(fileName, content) {
   const file = {
@@ -13,7 +16,7 @@ async function createFile(fileName, content) {
 
   if (error) {
     const { path } = error.details[0];
-    console.log(`Please specify ${path[0]} parameter`);
+    console.log(chalk.red(`Please specify ${path[0]} parameter`));
     return;
   }
 
@@ -21,7 +24,9 @@ async function createFile(fileName, content) {
 
   if (!result) {
     console.log(
-      `Sorry, this application doesn't support file with ${extension} extension`
+      chalk.red(
+        `Sorry, this application doesn't support file with ${extension} extension`
+      )
     );
     return;
   }
@@ -30,12 +35,49 @@ async function createFile(fileName, content) {
 
   try {
     await fs.writeFile(filePath, content, "utf-8");
-    console.log("file created successfully");
+    console.log(chalk.green("file created successfully"));
   } catch (err) {
-    console.log(err);
+    console.log(chalk.red(err));
   }
+}
+
+async function getFiles() {
+  const result = await fs.readdir(folderPath);
+
+  if (!result.length) {
+    console.log(chalk.red("Sorry, this folder is empty"));
+    return;
+  }
+
+  result.forEach((file) => {
+    console.log(chalk.green(file));
+  });
+}
+
+async function getFileInfo(fileName) {
+  const files = await fs.readdir(folderPath);
+
+  if (!files.includes(fileName)) {
+    console.log(chalk.red("Sorry, this file not found"));
+    return;
+  }
+
+  const filePath = path.join(folderPath, fileName);
+  const fileContent = await fs.readFile(filePath, "utf-8");
+  const fileInfo = await fs.stat(filePath);
+
+  const info = {
+    extension: path.extname(fileName),
+    name: path.basename(fileName, path.extname(fileName)),
+    content: fileContent,
+    date: fileInfo.birthtime.toString(),
+  };
+
+  console.log(info);
 }
 
 module.exports = {
   createFile,
+  getFiles,
+  getFileInfo,
 };
